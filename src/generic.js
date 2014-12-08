cf4cf460aab056f309c916994b6debcb552b0cfb
Prashant Sinha <prashnts@live.com>
2014-12-08 14:59:49 +0530

    DIJKSTRA

--- src/generic.js
+++ src/generic.js
@@ -1,72 +1,3 @@
-/*var dijkstra = function (adjacency, source, target) {
-    "use strict";
-
-    var dist = [],
-        sptSet = [],
-        previous = [],
-        i = 0,
-        minDistance = function () {
-            var min = Number.MAX_VALUE,
-                min_index,
-                v = 0;
-            for (v = 0; v < adjacency.length; v += 1) {
-                if (sptSet[v] === false && dist[v] < min) {
-                    min = dist[v];
-                    min_index = v;
-                }
-            }
-
-            return min_index;
-        };
-
-    for (i = 0; i < adjacency.length; i += 1) {
-        dist[i] = Number.MAX_VALUE;
-        sptSet[i] = false;
-        previous[i] = [];
-    }
-
-    dist[source] = 0;
-
-    for (i = 0; i < adjacency.length; i += 1) {
-        var u = minDistance(),
-            v;
-
-        sptSet[u] = true;
-
-        for (v = 0; v < adjacency.length; v+= 1) {
-            if (!sptSet[v] &&                               // If it isnt already visited
-                 adjacency[u][v] &&                         // If there IS a path
-                 dist[u] !== Number.MAX_VALUE &&            // If it isn't infinite.
-                 dist[u] + adjacency[u][v] < dist[v]) {     // Distance to this is less. THEN:
-                dist[v] = dist[u] + adjacency[u][v];
-                previous[i][v] = u;
-            }
-        }
-    }
-
-    var Path = [];
-
-    console.log(previous);
-    
-    for (i = 0; i < adjacency.length; i += 1) {
-        for (var j = 0; j < previous[i].length; j += 1) {
-            if (previous[i][j])
-                console.log(previous[i][j]);
-        }
-    }
-
-    return dist;
-};
-
-Object.length = function(obj) {
-    "use strict";
-    var size = 0, key;
-    for (key in obj) {
-        if (obj.hasOwnProperty(key)) size++;
-    }
-    return size;
-};*/
-
 function dijkstra (graph, source, destination, visited, distances, predecessors) {
     "use strict";
     if (!visited) {
@@ -85,16 +16,14 @@ function dijkstra (graph, source, destination, visited, distances, predecessors)
         var pred = destination;
         while (pred !== undefined) {
             path.push(pred);
-            pred = predecessors.pred;
-            console.log(predecessors);
+            pred = predecessors[pred];
         }
-        console.log("Result",path, "Distance", distances[destination]);
+        return {path: path, distance: distances[destination]};
     } else {
         if (visited.length === 0) {
             // Initial run
             distances[source] = 0;
         }
-
 
         for (var i = 0; i < Object.keys(graph[source]).length; i += 1) {
             var neighbour = Object.keys(graph[source])[i];
@@ -121,9 +50,7 @@ function dijkstra (graph, source, destination, visited, distances, predecessors)
         var arr = Object.keys( unvisited ).map(function ( key ) { return unvisited[key]; });
         var x = Object.keys(unvisited).filter(function(key) {return unvisited[key] === Math.min.apply( null, arr )})[0];
 
-        //console.log("ored", predecessors);
-
-        dijkstra(graph, x, destination, visited, distances, predecessors);
+        return dijkstra(graph, x, destination, visited, distances, predecessors);
     }
 };
 
@@ -151,4 +78,3 @@ graph = {
         'c': 3,
         'd': 5}};
 
-dijkstra(graph, 's', 'd');
